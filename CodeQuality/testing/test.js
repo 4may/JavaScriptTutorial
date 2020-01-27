b1215d1f05dd6ce2c375e7224380f978a374b5f3
Naoki Hanzawa <naoki.hanzawa.pub@gmail.com>
2020-01-27 20:56:33 +0900

    basic implementation

---
+++ CodeQuality/testing/test.js
@@ -0,0 +1,9 @@
+describe("pow", function()
+    {
+        //対象のユースケースを記載。人間が読める形式で書く。
+        it("raises to n-th power", function()
+        {
+            //テスト関数
+            assert.equal(pow(2, 3), 8);
+        });
+    });
